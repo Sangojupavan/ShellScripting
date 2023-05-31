addccf50d3f5a6615e219607ad62160bae1df73f
Pavan <sangoju@gmail.com>
2023-05-31 12:12:53 +0530

    added files

---
+++ hello/helloworld.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+########################################################################################################
+# Author:Sangoju Pavan Kumar
+# Version: v1.0.0
+# Date: 25-05-2023
+# Description: Simple shellscript of helloworld
+# Usage: print hello world (./helloworld.sh)
+########################################################################################################
+
+
+echo "hello world!"
