6914f0a342a2f7340c4bb0b804b0e718127fd749
Pavan <sangoju@gmail.com>
2023-05-31 17:03:40 +0530

    added files

--- hello/printname.sh
+++ hello/printname.sh
@@ -2,10 +2,27 @@
 ########################################################################################################
 # Author:Sangoju Pavan Kumar
 # Version: v1.0.0
-# Date: 25-05-2023
-# Description: Simple shellscript of helloworld
-# Usage: print hello world (./helloworld.sh)
+# Date: 30-05-2023
+# Description: Simple shellscript of printname
+# Usage: varibales usage (./variables.sh <pass name>)
 #######################################################################################################
 
+name="${1}"
+echo "hello,${1}"
 
-echo "hello world!"
+
+
+name="${1}${2}${3}"
+echo "hello,${1},${2},${3}"
+
+
+
+
+
+
+
+
+
+
+
+
--- hello/variables.sh
+++ hello/variables.sh
@@ -2,10 +2,18 @@
 ########################################################################################################
 # Author:Sangoju Pavan Kumar
 # Version: v1.0.0
-# Date: 25-05-2023
-# Description: Simple shellscript of helloworld
-# Usage: print hello world (./helloworld.sh)
+# Date: 30-05-2023
+# Description: Simple shellscript of variables
+# Usage: varibales usage (./variables.sh)
 #######################################################################################################
 
+name="Sangoju Pavan Kumar"
+organization="SSG Chary"
+echo "This Is ${name} my company name ${organization}"
 
-echo "hello world!"
+
+echo $((65+5))
+
+
+
+
